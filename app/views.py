5c9b3f058ba3c2737fc40eb26c3039bead4e06d0
ChrisAchinga <achinga.chris@gmail.com>
2021-03-13 21:37:59 +0300

    views and templates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
-def tasklist(request):
-    return HttpResponse('Task List')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'app/task.html' 
